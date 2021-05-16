90621f8bc6ff201518f6f97f814a8a17f8fd8576
Marc <RhinoDevel@users.noreply.github.com>
2021-05-16 16:37:02 +0200

    Simplify, don't use global object, correct comment.

--- room.js
+++ room.js
@@ -2,15 +2,15 @@
 // (c) Marcel Timm, RhinoDevel, 2021
 
 /** To be run during page load to augment global gamupet object with new
- *  properties called roomLateInit and room.
- *  roomLateInit() is a function to be run AFTER page load (to get correct body
- *  dimensions), room.init() must be run after that.
+ *  property called room.
+ *  room.init() is a function to be run AFTER page load (to get correct body
+ *  dimensions).
  */
 (function() // IIFE
 {
     'use strict';
 
-    var f = {}, c = {}, v = {};
+    var f = {}, c = {}, v = {}, o = {};
 
     c.dim = {};
 
@@ -43,23 +43,9 @@
     /**
      * - Alters given element's dimensions.
      */
-    f.init = function(ele)
+    f.init = function(p)
     {
-        v.bg = gamupet.ele.createCanvas(
-            0, 0, c.dim.bg.w, c.dim.bg.h, c.dim.bgAbs.w, c.dim.bgAbs.h);
-        ele.appendChild(v.bg);
-
-        ele.style.width = String(c.dim.bg.w) + 'px';
-        ele.style.height = String(c.dim.bg.h) + 'px';
-    };
-
-    /** To be run AFTER page load to augment global gamupet object with new
-     *  property called room, which is an object holding functions to create
-     *  and handle a dressing room.
-     */
-    gamupet.roomLateInit = function(width, height)
-    {
-        c.dim.bgAbs = { w: width, h: height };
+        c.dim.bgAbs = { w: p.width, h: p.height };
 
         c.pixelFactor = f.getPixelFactor();
         
@@ -67,8 +53,16 @@
             w: f.pixelFactor(c.dim.bgAbs.w), h: f.pixelFactor(c.dim.bgAbs.h)
         };
         
-        gamupet.room = {};
-        gamupet.room.init = f.init;
-        gamupet.room.getCanvas = f.getCanvas;
+        v.bg = p.createCanvas(
+            0, 0, c.dim.bg.w, c.dim.bg.h, c.dim.bgAbs.w, c.dim.bgAbs.h);
+        p.ele.appendChild(v.bg);
+
+        p.ele.style.width = String(c.dim.bg.w) + 'px';
+        p.ele.style.height = String(c.dim.bg.h) + 'px';
     };
+
+    o.init = f.init;
+    o.getCanvas = f.getCanvas;
+
+    gamupet.room = o;
 }());
