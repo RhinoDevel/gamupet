b0121a2af8dabbea56e4d0dfe0e7fa0b96535d4c
Marc <RhinoDevel@users.noreply.github.com>
2021-12-21 20:35:43 +0100

    Always use container element.

--- main.js
+++ main.js
@@ -9,38 +9,39 @@
 
     var f = {};
 
+    f.prepareBodyForFullScreen = function()
+    {
+        document.body.style.width = '100%';
+        document.body.style.height = '100%';
+        document.body.style.border = '0px none';
+        document.body.style.padding = '0px';
+        document.body.style.margin = '0px';
+    };
+
     f.onLoad = function()
     {
         var ele = document.createElement('div'),
             canvas = null,
-            container = null,
+            container = document.createElement('div'),
             outerDim = {};
+
+        document.body.appendChild(container);
 
         if(gamupet.c.fullscreen)
         {
-            container = document.body;
-
-            container.style.width = '100%';
-            container.style.height = '100%';
-            container.style.border = '0px none';
-            container.style.padding = '0px';
-            container.style.margin = '0px';
+            f.prepareBodyForFullScreen();
 
             outerDim.width = document.body.clientWidth;
             outerDim.height = document.body.clientHeight;
         }
         else
         {
-            container = document.createElement('div');
-
             outerDim.width = 640;
-            outerDim.height = 480;
-
-            container.style.width = String(outerDim.width) + 'px';
-            container.style.height =  String(outerDim.height) + 'px';
-
-            document.body.appendChild(container);
+            outerDim.height = 480;            
         }
+
+        container.style.width = String(outerDim.width) + 'px';
+        container.style.height = String(outerDim.height) + 'px';
 
         ele.style.order = String(1);
         ele.style.position = 'relative';
